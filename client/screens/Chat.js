1fbb86ed607f09e5cc35f28638f968525370efac
Mariam Batool <98965810+MariamBatool-git@users.noreply.github.com>
2022-05-22 18:58:36 +0500

    new chat.js file

--- client/screens/Chat.js
+++ client/screens/Chat.js
@@ -1,6 +1,6 @@
 //necessary imports
-import React, {Component, useEffect} from 'react';
-import { StyleSheet, Text, View, Image, Button, ScrollView, TouchableOpacity, Alert, Platform, KeyboardAvoidingView, ToastAndroid } from 'react-native';
+import React, {useState, useEffect, Component} from 'react';
+import { StyleSheet, Text, View, Image, Button, ScrollView, Platform, KeyboardAvoidingView, ToastAndroid } from 'react-native';
 import SpeechAndroid from 'react-native-android-voice';
 import uuid from "uuid";
 import Tts from 'react-native-tts';
@@ -8,8 +8,9 @@ import { Actions, GiftedChat, Bubble, Send, InputToolbar, Composer } from 'react
 import { Dialogflow_V2 } from 'react-native-dialogflow';
 //details of key for dialogflow integration
 import { dialogflowConfig } from '../env';
+import Card from './Card';
 import * as Permissions from 'expo-permissions';
-import axios from 'axios';
+
 
  
 // text variable to access dialogflow response outside of Chat component
@@ -25,24 +26,7 @@ const BOT_USER = {
 // counting the number of times dialogflow response was spoken
 var speak=0;
 
-const Card = (props)=>{
-  return(
-    <View style = {styles.cardContainer}>
-      <View style = {styles.imgContainer}>
-        <Image style = {styles.cardImg} source = {props.imagePath}/>
-      </View>
-      <View>
-        <Text style = {styles.heading}>{props.recipeName}</Text>
-      </View>
-      <View>
-        <Button 
-          title="Start Cooking"
-          color="#85586F"
-          />
-      </View>
-    </View>
-  );
-}
+
 const CardView = (props) =>{
   /*useEffect(()=>{
     this.getRecipe();
@@ -66,6 +50,220 @@ const CardView = (props) =>{
       </ScrollView>
     );
 }
+/*const Chat = () => {
+  const [messages, setMessages] = useState([
+    {
+      _id: 1,
+      text: `Hi! I am Recipebot, your personal cooking assistant.`,
+      createdAt: new Date(),
+      user: BOT_USER
+    }
+  ]);
+  const [dialogFlowResponse, setDialogFlowResponse] = useState('');
+  useEffect(()=>{
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id
+    );
+  },[]);
+  const handleGoogleResponse = (result)=>{
+    text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    sendBotResponse(text);
+  }
+  const sendBotResponse = (text) => {
+    let msg = {
+      _id: messages.length + 1,
+      text,
+      createdAt: new Date(),
+      user: BOT_USER
+    };
+    setMessages((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, [msg])
+    }));
+    if (speak) {
+      speak=0; 
+      Tts.getInitStatus().then(() => {
+        Tts.speak(text);
+      }, (err) => {
+        if (err.code === 'no_engine') {
+          Tts.requestInstallEngine();
+        }
+      });
+    }
+  }
+  const uuidGen = ()=>{
+    return uuid.v4();
+  }
+
+  const micHandler = async ()=>{
+    try{
+      let textSpeech = await SpeechAndroid.startSpeech("Speak now", SpeechAndroid.ENGLISH);
+      console.log(textSpeech);
+      console.log("heloo");
+      speak=1;
+      let StateVariable = [{
+        "text": textSpeech,
+        "user": {
+          "_id": 1
+        },
+        "createdAt": new Date(),
+        "_id": this.uuidGen()
+      }];
+      onSend(StateVariable);
+      ToastAndroid.show(textSpeech, ToastAndroid.LONG);
+      return(Promise.resolve(1));
+    }
+    catch (error){
+      switch (error) {
+        case SpeechAndroid.E_VOICE_CANCELLED:
+          
+          ToastAndroid.show("Voice Recognizer cancelled", ToastAndroid.LONG);
+          break;
+        case SpeechAndroid.E_NO_MATCH:
+          ToastAndroid.show("No match for what you said", ToastAndroid.LONG);
+          break;
+        case SpeechAndroid.E_SERVER_ERROR:
+          ToastAndroid.show("Google Server Error", ToastAndroid.LONG);
+          break;
+     }
+     return (Promise.reject("Cancelled"));
+    }
+  }
+  const onSend = (messages = [])=>{
+    setMessages((previousState) => ({ 
+      messages : GiftedChat.append(previousState.messages, messages)
+    }));
+    let message = messages[0].text;
+    Dialogflow_V2.requestQuery(
+      message,
+      result => handleGoogleResponse(result),
+      error => console.log(error)
+    );
+  }
+  const renderBubble =(props) => {
+    return (
+      <Bubble
+        {...props}
+        timeTextStyle={{
+          right: {
+            color: '#000000',
+          },
+        }}
+        textStyle={{
+          right: {
+            color: '#000000',
+          },
+        }}
+        wrapperStyle={{
+          left: {
+            alignSelf: 'stretch',
+            backgroundColor: '#F8ECD1',
+          },
+          right: {
+            backgroundColor: '#DEB6AB',
+          },
+        }}
+      />
+    );
+  }
+  const renderSend = (props) => {
+    if (props.text.trim().length > 0) {
+      return (
+        <Send
+            {...props}
+            containerStyle = {styles.sendcontainerStyle}
+          >
+            <Image
+              source={require('../assets/send.png')}
+              style={styles.sendLogoStyle}
+            />
+          </Send>
+      );
+    }
+  }
+  const renderInputToolbar = (props)=>{
+    return (
+      <InputToolbar
+       {...props}
+       containerStyle= {styles.containerStyle}/>
+       
+    )       
+  }
+  const renderCustomView = (props)=>{
+    const {currentMessage} = props;
+    console.log(currentMessage);
+    // specific recipe
+    if (currentMessage.text.indexOf(word) !== -1 && (currentMessage.text.toLowerCase().indexOf("pasta") !== -1)){
+      return(
+        <Card imagePath = {require('../assets/pasta.jpg')} recipeName = "Pasta" />
+      )
+    }
+    // cuisine based search
+    else if (currentMessage.text.indexOf(word) !== -1 && (currentMessage.text.toLowerCase().indexOf("pakistani") !== -1)){
+      return(
+        <CardView imagePath1 = {require('../assets/samosa.jpeg')} recipeName1 = "Chicken Samosa" 
+        imagePath2 = {require('../assets/keema.png')} recipeName2 = "Qeema"
+        imagePath3 = {require('../assets/halwa.jpeg')} recipeName3 = "Halwa" />
+      )
+    }
+    // mealtime based search
+    else if (currentMessage.text.indexOf(word) !== -1 && (currentMessage.text.toLowerCase().indexOf("dinner") !== -1)){
+      return(
+        <CardView imagePath1 = {require('../assets/keema.png')} recipeName1 = "Qeema" 
+        imagePath2 = {require('../assets/pizza.png')} recipeName2 = "Pizza"
+        imagePath3 = {require('../assets/biryani.png')} recipeName3 = "Biryani" />
+      )
+    }
+    // foodtype based search
+    else if (currentMessage.text.indexOf(word) !== -1 && (currentMessage.text.toLowerCase().indexOf("chicken") !== -1)){
+      return(
+        <CardView imagePath1 = {require('../assets/biryani.png')} recipeName1 = "Chicken Biryani" 
+        imagePath2 = {require('../assets/pasta.jpg')} recipeName2 = "Pizza"
+        imagePath3 = {require('../assets/pizza.png')} recipeName3 = "Pizza" />
+      )
+    }
+  }
+  const renderActions = (props) =>{
+    return (
+      <Actions
+        {...props}
+        containerStyle={{
+          width: 25,
+        }}
+        icon={() => (
+          <Image
+                source={require('../assets/MicIcon.png')}
+                style={styles.micIconStyle}
+              />
+        )}
+      />
+    );
+  }
+  return(
+    // flex : 1 the size of all of the other elements will have the same width as their content
+    <View style={{ flex: 1, backgroundColor: '#ffffff' }}>
+    <GiftedChat messages={messages}
+      placeholder = "Type your message here..."
+      renderBubble = {renderBubble}
+      renderSend = {renderSend}
+      renderInputToolbar = {renderInputToolbar}
+      renderCustomView = {renderCustomView}
+      isCustomViewBottom = {true}
+      onPressActionButton = {micHandler}
+      renderActions = {renderActions}
+      onSend = {messages => onSend(messages)}
+      user = {{_id : 1}}
+      {
+        ...
+        Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
+      }
+    />
+  </View>
+  )
+
+}*/
 class Chat extends Component{
   state = {
     messages : [
